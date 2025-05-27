305a8b92e414b17fab04b32ef5ae14de15f13c30
mlzayn <79521470188@yandex.ru>
2025-05-27 10:09:08 +0500

    func

--- Django/django_project/main/static/js/script1.js
+++ Django/django_project/main/static/js/script1.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", function() {
     let tableMassiv = document.getElementById('tableMassiv');
     let tableBody = this.document.getElementById('data-output');
 
-    document.getElementById('tableMassiv').addEventListener('click', function(e) {
+    document.getElementById('tableMassiv').addEventListener('dblclick', function(e) {
     // Проверяем, что кликнули по ячейке таблицы
     if (e.target.tagName === 'TD') {
         // Получаем родительскую строку
@@ -17,22 +17,23 @@ window.addEventListener("DOMContentLoaded", function() {
         quantity: cells[1].textContent,
         };
         
+        const modal = new bootstrap.Modal(document.getElementById('change-modal'));
+
         // Заполняем модальное окно
-        const nameField = document.getElementById('name');
-        const quantityField = document.getElementById('quantity');
+        const nameField = document.getElementById('changeName');
+        const quantityField = document.getElementById('changeQuantity');
 
         nameField.value = data.name;
         quantityField.value = data.quantity
         console.log(nameField.value);
         
         // Показываем модальное окно
-        const modal = new bootstrap.Modal(document.getElementById('change-modal'));
         modal.show();
     }
     });
 
-    const response =  fetch('',
-        {
+    function createTable(tableBody) {
+        fetch('', {
             method: 'PATCH',
             cache: 'no-cache',
             credentials: 'same-origin',
@@ -49,7 +50,31 @@ window.addEventListener("DOMContentLoaded", function() {
                 tr.insertCell().innerHTML = row[1];
                 tableBody.appendChild(tr);
                 }
-            })
+                })
+            }
+
+        createTable(tableBody);
+    
+
+    // const response =  fetch('',
+    //     {
+    //         method: 'PATCH',
+    //         cache: 'no-cache',
+    //         credentials: 'same-origin',
+    //         headers: {
+    //             'Content-Type': 'application/json: charset-utf-8',
+    //             'X-CSRFToken': document.querySelector('[name="csrfmiddlewaretoken"]').value,
+    //         }
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         for(var row of data){
+    //             const tr = document.createElement('tr')
+    //             tr.insertCell().innerHTML = row[0];
+    //             tr.insertCell().innerHTML = row[1];
+    //             tableBody.appendChild(tr);
+    //             }
+    //         })
 
 
     const addBtn = document.querySelector(".addBtn1")
@@ -68,7 +93,7 @@ window.addEventListener("DOMContentLoaded", function() {
             credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json: charset-utf-8',
-                'X-CSRFToken': document.querySelector('[name-"csrfmiddlewaretoken"]').value,
+                'X-CSRFToken': document.querySelector('[name="csrfmiddlewaretoken"]').value,
             }
         })
         .then(response => response.json())
@@ -82,7 +107,19 @@ window.addEventListener("DOMContentLoaded", function() {
             })
         });
 
-    })
+    let row = null;
+    document.getElementById('tableMassiv').addEventListener('click', event => {
+            let r = event.target.closest('tr');
+            if (!r || r === row)
+            return;
+            
+            if (row)
+            delete row.dataset.selected;
+            
+            (row = r).dataset.selected = true;
+        });
+
+ });
 
 
 
